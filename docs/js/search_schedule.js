769cc0197392e9e4d12b3e4de8c423c252dd79aa
flyingsc <*>
2017-04-04 23:13:59 +0900

    モバイル対応

--- docs/js/search_schedule.js
+++ docs/js/search_schedule.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
-    var now = new Date();
-    $("td.date").each(function(){
-	var date = $(this).html();
-	var a = date.split(" ")[0].split("/");
-	var mon = parseInt(a[0]);
-	var day = parseInt(a[1]);
-
-	if(mon < now.getMonth() + 1 || day < now.getDate()){
-	    $(this).parent().remove();
-	}
-    });
-    
     $("select#genre").change(function(){
 	var genre = $(this).val();
+	var date_show_flag = true;
+	var prev_date = 0;
 	
 	$("select#tournament").val("全て");
+
 	$("select#tournament>option").each(function(){
 	    if($(this).attr("class") == genre || genre == "全て" || $(this).val() == "全て"){
 		$(this).show();
@@ -29,6 +20,19 @@ $(document).ready(function(){
 	    
 	    if(txt == genre || genre == "全て" || txt == undefined){
 		$(this).show();
+
+		if($(this).attr("class") == "date-row"){
+		    if(date_show_flag == false){
+			$(prev_date).hide();
+		    }
+
+		    date_show_flag = false;
+		    prev_date = this;
+		    count = 0;
+		    return true;
+		}
+
+		date_show_flag = true;
 		
 		if(count % 2 == 0){
 		    $(this).css("background-color", "#ffffff");
@@ -41,21 +45,38 @@ $(document).ready(function(){
 		$(this).hide();
 	    }
 	});
+
+	if(date_show_flag == false){
+	    $(prev_date).hide();
+	}
     });
 
     $("select#tournament").change(function(){
-	//$("select#genre").val("全て");
-	
 	var tournament = $(this).val();
 	var genre = $("select#genre").val();
 	var count = 0;
-
+	var date_show_flag = true;
+	var prev_date = 0;
+	
 	$("tr").each(function(){
 	    var txt = $(this).find("td.tournament").html();
 
 	    if(txt == tournament || (tournament == "全て" && (genre == "全て" || genre == $(this).find("td.genre").html())) || txt == undefined){
 		$(this).show();
 		
+		if($(this).attr("class") == "date-row"){
+		    if(date_show_flag == false){
+			$(prev_date).hide();
+		    }
+
+		    date_show_flag = false;
+		    prev_date = this;
+		    count = 0;
+		    return true;
+		}
+
+		date_show_flag = true;
+		
 		if(count % 2 == 0){
 		    $(this).css("background-color", "#ffffff");
 		} else {
@@ -67,5 +88,9 @@ $(document).ready(function(){
 		$(this).hide();
 	    }
 	});
+
+	if(date_show_flag == false){
+	    $(prev_date).hide();
+	}
     });
 });
