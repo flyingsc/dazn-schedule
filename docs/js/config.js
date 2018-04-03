2282d7826803b02e0b3c233c8152cd61fd1b8ddf
flyingsc <*>
2018-04-03 10:10:57 +0900

    jsonリンク修正

--- docs/js/config.js
+++ docs/js/config.js
@@ -36,7 +36,7 @@ $(function(){
 	return;
     }
     
-    $.getJSON("./js/genre_tournament.json", null, function(data, status){
+    $.getJSON("https://flyingsc.github.io/dazn-schedule/js/genre_tournament.json", null, function(data, status){
 	var genre_tournament = data;
 	var saved_genre = [];
 	var saved_tournament = [];
